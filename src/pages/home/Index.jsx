5d722d3e9ff61847fffbb90c2070401cfeb28741
Farhan Ajmal <farhanajmal789@gmail.com>
2023-06-23 00:40:41 -0700

    fixed responsiveness issues in  home page

--- src/pages/home/Index.jsx
+++ src/pages/home/Index.jsx
@@ -7,13 +7,15 @@ import charjDesktop from "../../assets/images/charj-desktop.jpg";
 import ecoSystem from "../../assets/images/eco-system.jpg";
 import { useMediaQuery } from 'react-responsive';
 import { Link } from "react-router-dom";
+import Footer from "../../components/footer";
 
 const Home = () => {
   const isSmallScreen = useMediaQuery({ maxWidth: 780 });
   const products = [
     {
       id: 1,
-      videoSrc:electricVehicle ,
+      videoSrc: electricVehicle,
+      background: true,
       title: "Cross Continent EV Revolution",
       subTitle: "Drive All Roads in the US, Canada, Europe and Middle East",
       orderUrl: "https://www.tesla.com/model3/design#overview",
@@ -22,6 +24,8 @@ const Home = () => {
     {
       id: 2,
       videoSrc: heroVideo,
+      background: true,
+
       title: "Cross Continent EV Revolution",
       subTitle: "Drive All Roads in the US, Canada, Europe and Middle East",
       orderUrl: "https://www.tesla.com/model3/design#overview",
@@ -30,8 +34,8 @@ const Home = () => {
     {
       id: 3,
       imgSrc: charjDesktop,
-      title: "Cross Continent EV Revolution",
-
+      title: "Types of EV Chargers",
+      background: false,
       imgSrcMobile: charjMobile2,
       imgSrcDesktop: true,
       orderUrl: "https://www.tesla.com/model3/design#overview",
@@ -40,7 +44,7 @@ const Home = () => {
     {
       id: 4,
       imgSrcMobile: charjMobile2,
-
+      background: false,
       orderUrl: "https://www.tesla.com/model3/design#overview",
       demoUrl: "https://www.tesla.com/drive?selectedModel=model3",
     },
@@ -54,7 +58,7 @@ const Home = () => {
       id: 6,
       imgSrc: ecoSystem,
       imgSrcMobile: ecoSystem,
-
+      background: false,
       orderUrl: "https://www.tesla.com/model3/design#overview",
       demoUrl: "https://www.tesla.com/drive?selectedModel=model3",
     },
@@ -66,34 +70,47 @@ const Home = () => {
         const hasVideo = !!item.videoSrc;
 
         if (!hasImage && !hasVideo) {
-          return null; 
+          return null;
         }
         return (
           <div className="h-[100vh] max-w-screen relative snap-center" key={index}>
-            <div className="relative flex justify-center items-center h-screen w-full">
-              {isSmallScreen && item.imgSrcMobile ? (
-                <img src={item.imgSrcMobile} alt="" className="h-[75vh] w-screen object-contain" />
-              ) : (
-                item.imgSrc && <img src={item.imgSrc} alt="" className="h-[80vh] w-screen object-contain" />
-              )}
-              {item.videoSrc && <video src={item.videoSrc} autoPlay loop muted className="h-full w-screen object-cover" />}
-              <div className="absolute top-20 w-full flex flex-col gap-3 text-center justify-center items-center text-white">
-                {item.id != 3 && item.title && <h1 className="text-4xl font-bold z-10">{item.title}</h1>}
-                {item.id === 3 && item.title && <h1 className="text-xl md:text-2xl font-bold z-10 text-black ">{item.title}</h1>}
-                {item.subTitle && <p className="text-sm font-medium z-10 mt-5">{item.subTitle}</p>}
-              </div>
-              <div className="absolute w-full bottom-20 flex flex-col md:flex-row gap-3 text-center justify-center items-center z-10">
-                {item.id === 1 ? (
-                  <Link to="/services" className="bg-gray-100 px-20 py-1.5 text-black text-md rounded-sm" >
-                    Our Services
-                  </Link>
+            {item.background ? (
+              <div className="relative flex justify-center items-center h-screen w-full">
+                {isSmallScreen && item.imgSrcMobile ? (
+                  <img src={item.imgSrcMobile} alt="" className="h-[75vh] w-screen object-contain" />
                 ) : (
-                 ""
+                  item.imgSrc && <img src={item.imgSrc} alt="" className="h-[80vh] w-screen object-contain" />
+                )}
+                {item.videoSrc && <video src={item.videoSrc} autoPlay loop muted className="h-full w-screen object-cover" />}
+                <div className="absolute top-20 w-full flex flex-col gap-3 text-center justify-center items-center text-white">
+                  {item.title && <h1 className="text-4xl font-bold text-white z-10">{item.title}</h1>}
+                  {item.subTitle && <p className="text-sm font-medium z-10 mt-5">{item.subTitle}</p>}
+                </div>
+                <div className="absolute w-full bottom-20 flex flex-col md:flex-row gap-3 text-center justify-center items-center z-10">
+                  {item.id === 1 ? (
+                    <Link to="/services" className="bg-gray-100 px-20 py-1.5 text-black text-md rounded-sm" >
+                      Our Services
+                    </Link>
+                  ) : (
+                    ""
+                  )}
+                </div>
+              </div>
+            ) : (
+              <div className="grid gap-0 lg:gap-2 text-center pt-20">
+                <div>
+                  {item.title && <h1 className="text-2xl md:text-4xl  font-bold text-black z-10">{item.title}</h1>}
+                  {item.subTitle && <p className="text-sm font-medium z-10 mt-5">{item.subTitle}</p>}
+                </div>
+                {isSmallScreen && item.imgSrcMobile ? (
+                  <img src={item.imgSrcMobile} alt="" className="h-[80vh] w-screen object-contain" />
+                ) : (
+                  item.imgSrc && <img src={item.imgSrc} alt="" className="h-[80vh] md:h-auto lg:h-[80vh] w-screen object-contain" />
                 )}
               </div>
-            </div>
+            )}
 
-           
+
           </div>
         );
       })}
